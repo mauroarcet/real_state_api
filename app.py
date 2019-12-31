478dc2382e91f9e457a7511dbc2a518b2859aa0a
Mauricio Arce <mau@conekta.com>
2019-12-31 11:13:01 -0600

    feature/ Add Flask app with AWS RDS db and SQLAlchemy ORM

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from flask_marshmallow import Marshmallow
+from ma import ma
+
+connection_string = (
+    "postgresql"
+    + "+psycopg2"
+    + "://"
+    + "postgres:"
+    + "^kq55QAuIwr#"
+    + "@real-state-dev-db.canrzxdvzoof.us-east-2.rds.amazonaws.com"
+    + ":5432"
+    + "/"
+    + "realStateDb"
+)
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.secret_key = "mau"
+api = Api(app)
+
+# Run first time in order to create the db to be used by the API
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(RealState, "/real_state", "/real_state/<string:id>")
+api.add_resource(RealStateList, "/real_states")
+api.add_resource(UserRegister, "/register")
+
+if __name__ == "__main__":
+    from db import db
+
+    db.init_app(app)
+    ma.init_app(app)
+    app.run(port=5000, debug=True)
